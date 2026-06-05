326cfce8613196ad77e4fb1c2e782f6d5c4b438b
Tan <kaitan@amd.com>
2026-06-05 13:22:18 -0400

    refactor blueprint for code dedupe

--- Source/Schola/Public/Common/BlueprintErrorUtils.h
+++ Source/Schola/Public/Common/BlueprintErrorUtils.h
@@ -37,19 +37,7 @@ inline void RaiseBlueprintError(const FString& InFunctionName, const FString& In
  */
 inline void RaiseInvalidInstancedStructError(const FString& InFunctionName)
 {
-#if !(UE_BUILD_TEST || UE_BUILD_SHIPPING)
-	FFrame* TopFrame = FFrame::GetThreadLocalTopStackFrame();
-	if (TopFrame)
-	{
-		const FString ErrorMessage = FString::Printf(TEXT("%s: Invalid InstancedStruct passed."), *InFunctionName);
-	#if WITH_EDITOR
-		const FBlueprintExceptionInfo ExceptionInfo(EBlueprintExceptionType::NonFatalError, FText::FromString(ErrorMessage));
-		FBlueprintCoreDelegates::ThrowScriptException(TopFrame->Object, *TopFrame, ExceptionInfo);
-	#else
-		UE_LOG(LogBlueprintUserMessages, Error, TEXT("%s:\n%s"), *ErrorMessage, *TopFrame->GetStackTrace());
-	#endif	// WITH_EDITOR
-	}
-#endif	// !(UE_BUILD_TEST || UE_BUILD_SHIPPING)
+	RaiseBlueprintError(InFunctionName, TEXT("Invalid InstancedStruct passed."));
 }
 
 /**
@@ -62,20 +50,8 @@ inline void RaiseInvalidInstancedStructError(const FString& InFunctionName)
 template<typename T>
 inline void RaiseInstancedStructTypeMismatchError(const TInstancedStruct<T>& InStruct, const FString& ExpectedType, const FString& InFunctionName)
 {
-#if !(UE_BUILD_TEST || UE_BUILD_SHIPPING)
-	FFrame* TopFrame = FFrame::GetThreadLocalTopStackFrame();
-	if (TopFrame)
-	{
-		const FString ErrorMessage = FString::Printf(TEXT("%s: Type mismatch. Expected %s but received %s."), 
-			*InFunctionName, *ExpectedType, *InStruct.GetScriptStruct()->GetName());
-	#if WITH_EDITOR
-		const FBlueprintExceptionInfo ExceptionInfo(EBlueprintExceptionType::NonFatalError, FText::FromString(ErrorMessage));
-		FBlueprintCoreDelegates::ThrowScriptException(TopFrame->Object, *TopFrame, ExceptionInfo);
-	#else
-		UE_LOG(LogBlueprintUserMessages, Error, TEXT("%s:\n%s"), *ErrorMessage, *TopFrame->GetStackTrace());
-	#endif	// WITH_EDITOR
-	}
-#endif	// !(UE_BUILD_TEST || UE_BUILD_SHIPPING)
+	RaiseBlueprintError(InFunctionName, FString::Printf(TEXT("Type mismatch. Expected %s but received %s."),
+		*ExpectedType, *InStruct.GetScriptStruct()->GetName()));
 }
 
 /**
@@ -84,20 +60,8 @@ inline void RaiseInstancedStructTypeMismatchError(const TInstancedStruct<T>& InS
  * @param ExpectedType The expected type name.
  * @param InFunctionName The name of the function where the error occurred.
  */
- inline void RaiseInstancedStructTypeMismatchError(const FInstancedStruct& InStruct, const FString& ExpectedType, const FString& InFunctionName)
- {
- #if !(UE_BUILD_TEST || UE_BUILD_SHIPPING)
-	 FFrame* TopFrame = FFrame::GetThreadLocalTopStackFrame();
-	 if (TopFrame)
-	 {
-		 const FString ErrorMessage = FString::Printf(TEXT("%s: Type mismatch. Expected %s but received %s."), 
-			 *InFunctionName, *ExpectedType, *InStruct.GetScriptStruct()->GetName());
-	 #if WITH_EDITOR
-		 const FBlueprintExceptionInfo ExceptionInfo(EBlueprintExceptionType::NonFatalError, FText::FromString(ErrorMessage));
-		 FBlueprintCoreDelegates::ThrowScriptException(TopFrame->Object, *TopFrame, ExceptionInfo);
-	 #else
-		 UE_LOG(LogBlueprintUserMessages, Error, TEXT("%s:\n%s"), *ErrorMessage, *TopFrame->GetStackTrace());
-	 #endif	// WITH_EDITOR
-	 }
- #endif	// !(UE_BUILD_TEST || UE_BUILD_SHIPPING)
- }
+inline void RaiseInstancedStructTypeMismatchError(const FInstancedStruct& InStruct, const FString& ExpectedType, const FString& InFunctionName)
+{
+	RaiseBlueprintError(InFunctionName, FString::Printf(TEXT("Type mismatch. Expected %s but received %s."),
+		*ExpectedType, *InStruct.GetScriptStruct()->GetName()));
+}

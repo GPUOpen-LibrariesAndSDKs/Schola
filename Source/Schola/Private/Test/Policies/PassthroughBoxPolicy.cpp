1ed4f5239357628444cc6f5622f86a964f4984c2
Alexander Cann <Alexander.Cann@amd.com>
2026-06-17 11:21:28 -0400

    fix(inference): Removed a copy from BatchedThink

---
+++ Source/Schola/Private/Test/Policies/PassthroughBoxPolicy.cpp
@@ -0,0 +1,27 @@
+// Copyright (c) 2025 Advanced Micro Devices, Inc. All Rights Reserved.
+
+#include "Test/Policies/PassthroughBoxPolicy.h"
+
+#include "Points/BoxPoint.h"
+
+bool UPassthroughBoxPolicy::Think(const TInstancedStruct<FPoint>& InObservations, TInstancedStruct<FPoint>& OutAction)
+{
+	const FBoxPoint* Src = InObservations.GetPtr<FBoxPoint>();
+	if (!Src)
+	{
+		return false;
+	}
+	OutAction.InitializeAs<FBoxPoint>(Src->Values, Src->Shape);
+	return true;
+}
+
+bool UPassthroughBoxPolicy::Init(const FInteractionDefinition& InPolicyDefinition)
+{
+	(void)InPolicyDefinition;
+	return true;
+}
+
+bool UPassthroughBoxPolicy::IsInferenceBusy() const
+{
+	return false;
+}
---
+++ Source/Schola/Private/Test/Policies/PassthroughBoxPolicy.h
@@ -0,0 +1,23 @@
+// Copyright (c) 2025 Advanced Micro Devices, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "Policies/PolicyInterface.h"
+#include "PassthroughBoxPolicy.generated.h"
+
+/**
+ * Minimal policy for tests: copies FBoxPoint observations to actions.
+ * Uses the default IPolicy::BatchedThink (per-element Think).
+ */
+UCLASS()
+class UPassthroughBoxPolicy : public UObject, public IPolicy
+{
+	GENERATED_BODY()
+
+public:
+	bool Think(const TInstancedStruct<FPoint>& InObservations, TInstancedStruct<FPoint>& OutAction) override;
+	bool Init(const FInteractionDefinition& InPolicyDefinition) override;
+	bool IsInferenceBusy() const override;
+};

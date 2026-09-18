9960354a825633094e12a7cac5f8c501ba5038c7
Alexander Cann <Alexander.Cann@amd.com>
2026-09-18 10:47:35 -0400

    chore: applied black formatting

--- Test/lerobot/__init__.py
+++ Test/lerobot/__init__.py
@@ -1,6 +1,7 @@
 import pytest
+
 try:
-    import lerobot # directly imported in one of the tests modules.
-    import draccus # directly imported in one of the tests modules.
+    import lerobot  # directly imported in one of the tests modules.
+    import draccus  # directly imported in one of the tests modules.
 except ImportError:
     pytest.skip("lerobot not installed", allow_module_level=True)

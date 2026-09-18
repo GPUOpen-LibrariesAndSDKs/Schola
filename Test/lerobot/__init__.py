91c1b170d99a920d76cebca1ac5f31ce461c09c4
Alexander Cann <Alexander.Cann@amd.com>
2026-09-18 10:19:08 -0400

    chore: skip lerobot test module if lerobot and draccus are not installed.

---
+++ Test/lerobot/__init__.py
@@ -0,0 +1,6 @@
+import pytest
+try:
+    import lerobot # directly imported in one of the tests modules.
+    import draccus # directly imported in one of the tests modules.
+except ImportError:
+    pytest.skip("lerobot not installed", allow_module_level=True)
